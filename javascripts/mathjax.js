95de038ae75315f8d58344c537c84b3f958f856d
ksopyla <ksopyla@users.noreply.github.com>
2025-05-24 17:14:21 +0000

    deploy: 6fe4d35e0b5b00b5b468cf9de5bdfa2b99a758d9

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -1,13 +1,23 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]],
+    inlineMath: [["\\(", "\\)"], ["$", "$"]],
+    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
     processEscapes: true,
-    processEnvironments: true
+    processEnvironments: true,
+    processRefs: true
   },
   options: {
     ignoreHtmlClass: ".*|",
     processHtmlClass: "arithmatex"
+  },
+  startup: {
+    ready: () => {
+      MathJax.startup.defaultReady();
+      MathJax.startup.promise.then(() => {
+        // Process any math that might be in Jupyter notebook cells
+        console.log('MathJax startup complete');
+      });
+    }
   }
 };
 
